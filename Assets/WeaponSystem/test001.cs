17f944f75a5f2e9c24425c0260c1b10790f1e4ef
園田 <e184223sk@kurume-it.ac.jp>
2021-07-18 19:42:40 +0900

    test001　修正

--- Assets/WeaponSystem/test001.cs
+++ Assets/WeaponSystem/test001.cs
@@ -7,16 +7,16 @@ public class test001 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ChangeWeapon.SetLeft0(Instantiate(Resources.Load("M4")) as GameObject);
-        ChangeWeapon.SetLeft1(Instantiate(Resources.Load("BeamSaber")) as GameObject);
-        ChangeWeapon.SetRight0(Instantiate(Resources.Load("M4")) as GameObject);
-        ChangeWeapon.SetRight1(Instantiate(Resources.Load("BeamSaber")) as GameObject);
+        ChangeWeapon.SetLeft0(Instantiate(Resources.Load("Weapon/M4")) as GameObject);
+        ChangeWeapon.SetLeft1(Instantiate(Resources.Load("Weapon/BeamSaber")) as GameObject);
+        ChangeWeapon.SetRight0(Instantiate(Resources.Load("Weapon/M4")) as GameObject);
+        ChangeWeapon.SetRight1(Instantiate(Resources.Load("Weapon/BeamSaber")) as GameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (VRInput.A) ChangeWeapon.ChangeL();
-        if (VRInput.Y) ChangeWeapon.ChangeR();
+        if (VRInput.B) ChangeWeapon.ChangeR();
+        if (VRInput.Y) ChangeWeapon.ChangeL();
     }
 }
