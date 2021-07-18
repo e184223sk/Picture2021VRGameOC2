9094589f77d5a2071fecdcca8841a30fbc824147
akano-shingo <e214101as@kurume-it.ac.jp>
2021-07-18 21:46:05 +0900

    お手上げ＼(＾o＾)／

--- Assets/GameUI/UIControl.cs
+++ Assets/GameUI/UIControl.cs
@@ -10,11 +10,16 @@ public class UIControl : MonoBehaviour
     public RawImage ability1;
     public RawImage ability2;
     public RawImage ability3;
-    Image[] Weapon = new Image[30];
+    /*Image[] Weapon = new Image[30];
     Image[] ability = new Image[30];
-    public Text time;
-    public Text Reload;
-    public Text Point;
+        for(int e = 0;e< 30;e++)
+        {
+            Weapon[e] = GameObject.Find("Canvas/Weapon" + e).GetComponent<Image>();
+            ability[e] = GameObject.Find("Canvas/ability" + e).GetComponent<Image>();
+        }*/
+    Text time;
+    Text Reload;
+    Text Point;
     public Text Barrage;
     public bool[] IsGetw = new bool[30];
     public bool[] IsGeta = new bool[30];
@@ -22,45 +27,28 @@ public class UIControl : MonoBehaviour
     public bool rk, lk;
     public int[] IsBarrage;
     public int a = 100, s = 3, d = 500, f = 5;
-    int min;
-    float sec, Osec;
+    public float TimeCount = 60.0f;
     public WeaponData[] DataBase;
     public AbilityData[] database;
     void Start()
     {
-        /*for(int e = 0;e < 30;e++)
-        {
-            Weapon[e] = GameObject.Find("Canvas/Weapon" + e).GetComponent<Image>();
-            ability[e] = GameObject.Find("Canvas/ability" + e).GetComponent<Image>();
-        }*/
         Barrage = GameObject.Find("Canvas/Barrage").GetComponent<Text>();
         RWeapon = GameObject.Find("Canvas/RWeapon").GetComponent<RawImage>();
         LWeapon = GameObject.Find("Canvas/LWeapon").GetComponent<RawImage>();
         ability1 = GameObject.Find("Canvas/AbilityColumn/Ability1").GetComponent<RawImage>();
         ability2 = GameObject.Find("Canvas/AbilityColumn/Ability2").GetComponent<RawImage>();
         ability3 = GameObject.Find("Canvas/AbilityColumn/Ability3").GetComponent<RawImage>();
-        time = GameObject.Find("Canvas/Time").GetComponent<Text>();
+        time = GameObject.Find("Canvas/time").GetComponent<Text>();
         Point = GameObject.Find("Canvas/Point").GetComponent<Text>();
         Reload = GameObject.Find("Canvas/Reload").GetComponent<Text>();
-        min = 0;
-        sec = 0f;
-        Osec = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         //時間(未完了)
-        sec += Time.deltaTime;
-        if (sec >= 60f)
-        {
-            min++;
-            sec = sec - 60;
-        }
-        if ((int)sec != (int)Osec)
-        {
-            time.text = "TIME ：" + min.ToString("00") + ":" + ((int)sec).ToString("00");
-        }
+        time.text = string.Format("Time: {0:00.00}", TimeCount);
+        TimeCount -= Time.deltaTime;
         //ポイント(未完了)
         Point.text = "POINT ：" + a * s * d * f;
         //リロード(完了)
@@ -93,12 +81,21 @@ public class UIControl : MonoBehaviour
             }
             else
             {
-                RWeapon = null;
-                LWeapon = null;
+                RWeapon.texture = null;
+                LWeapon.texture = null;
             }
-            if (IsGeta[t] == true) { ability1.texture = database[t].image; }
-            if (IsGeta[t] == true) { ability2.texture = database[t].image; }
-            if (IsGeta[t] == true) { ability3.texture = database[t].image; }
+            if (IsGeta[t] == true)
+            {
+                ability1.texture = database[t].image;
+                ability2.texture = database[t].image;
+                ability3.texture = database[t].image;
+            }
+            else
+            {
+                ability1.texture = null;
+                ability2.texture = null;
+                ability3.texture = null;
+            }
         }
         
     }
