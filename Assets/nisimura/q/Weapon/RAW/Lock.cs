dc0b58a248c61fc2f8c98522a548cf0846b9ee97
e194140nh <e194140nh@kurume-it.ac.jp>
2021-07-18 21:33:53 +0900

    これでテクスチャが表示するはず！

---
+++ Assets/nisimura/q/Weapon/RAW/Lock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lock : MonoBehaviour
+{
+    /*[SerializeField]
+    Weapon _myWeapon;
+
+
+    public Transform ring, Obj; 
+
+    void Update()
+    {
+        ring.LookAt(Camera.main.transform);
+        Obj.Rotate(Vector3.up * Time.deltaTime * 60, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        WeaponManager._HavingWeapon = _myWeapon;
+        this.gameObject.SetActive(false);
+    }*/
+}
