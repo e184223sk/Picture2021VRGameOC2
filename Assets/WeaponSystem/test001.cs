07a2c5f0f93fabe77a34ea8ee0a6133635d30a3a
ikeda_ <ikeda.yuichiro.w.00@gmail.com>
2021-07-18 19:35:13 +0900

    切り替え

---
+++ Assets/WeaponSystem/test001.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test001 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeWeapon.SetLeft0(Instantiate(Resources.Load("M4")) as GameObject);
+        ChangeWeapon.SetLeft1(Instantiate(Resources.Load("BeamSaber")) as GameObject);
+        ChangeWeapon.SetRight0(Instantiate(Resources.Load("M4")) as GameObject);
+        ChangeWeapon.SetRight1(Instantiate(Resources.Load("BeamSaber")) as GameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (VRInput.A) ChangeWeapon.ChangeL();
+        if (VRInput.Y) ChangeWeapon.ChangeR();
+    }
+}
