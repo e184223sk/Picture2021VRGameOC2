d96d10e7d07d2c1956b631edd5b64719552772d1
ikeda_ <ikeda.yuichiro.w.00@gmail.com>
2021-07-19 12:44:42 +0900

    ロビー画面1

---
+++ Assets/LobbyScene/Materials/Lobby_ResetPanelArea.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Lobby_ResetPanelArea : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.GetComponent<Dammy_PLayer>() != null)
+            GameObject.Find("reset").GetComponent<Reset>().reborn();
+    }
+}
