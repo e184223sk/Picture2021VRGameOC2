d1af1997f6ae34ee1220e5f630c39c42baa68b6d
nodayuta_214136 <e214136ny@kurume-it.ac.jp>
2021-07-20 17:44:23 +0900

    Result画面がほとんど終わりました

---
+++ Assets/Result/Return.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Return : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    //ロビー画面へ遷移
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("ロビー画面へ行けた");
+            SceneManager.LoadScene("Lobby");
+        }   
+    }
+}
---
+++ Assets/Result/Shougou.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shougou : MonoBehaviour
+{
+    //定義
+    Text shougou;
+
+    //称号テキストの取得
+    void Start()
+    {
+        shougou = GameObject.Find("Shougou").GetComponent<Text>();
+    }
+
+    //表示
+    void Update()
+    {
+        if (BreakData.BreakingPercentage < 10) shougou.text = ("見習いの破壊者");
+        else if (BreakData.BreakingPercentage < 20) shougou.text = ("さすらいの破壊者");
+        else if (BreakData.BreakingPercentage < 30) shougou.text = ("一人前の破壊者");
+        else if (BreakData.BreakingPercentage < 40) shougou.text = ("破壊の達人");
+        else if (BreakData.BreakingPercentage < 50) shougou.text = ("破壊を極めし者");
+        else if (BreakData.BreakingPercentage < 60) shougou.text = ("破壊の創造者");
+        else if (BreakData.BreakingPercentage < 70) shougou.text = ("天下に轟く破壊者");
+        else if (BreakData.BreakingPercentage < 80) shougou.text = ("伝説の破壊者");
+        else if (BreakData.BreakingPercentage < 90) shougou.text = ("破壊王");
+        else if (BreakData.BreakingPercentage < 100) shougou.text = ("破壊神");
+        else shougou.text = ("究極の破壊神");
+    }
+}
