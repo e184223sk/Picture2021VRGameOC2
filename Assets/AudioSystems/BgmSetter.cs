c36c8d3bffb9285ffb02e07c1cfa9ba86e77f95d
ikeda_ <ikeda.yuichiro.w.00@gmail.com>
2021-07-27 11:04:25 +0900

    BGM関連システムと仮音源追加

---
+++ Assets/AudioSystems/BgmSetter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BgmSetter : MonoBehaviour
+{
+    int v;
+    AudioSource output;
+
+    [SerializeField]
+    AudioClip[] SongData;
+
+    void Start()
+    {
+        v = -1;
+        output = gameObject.AddComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!output.isPlaying)
+        {
+            v++;
+            if (SongData.Length == v) v = 0;
+            output.clip = SongData[v];
+            output.Play();
+        }
+    }
+}
