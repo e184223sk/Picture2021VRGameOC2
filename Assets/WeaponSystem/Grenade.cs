ed7ba873e7dd297effe519d8484c6a85f378ba1f
ikeda_ <ikeda.yuichiro.w.00@gmail.com>
2021-07-30 14:47:43 +0900

    6つとりあえず配置

---
+++ Assets/WeaponSystem/Grenade.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    void Start() => Invoke("_Bomb_", 5);
+
+    void _Bomb_()
+    {
+        Instantiate(Resources.Load("Bomb"), transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/WeaponSystem/GrenadeGen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeGen : MonoBehaviour
+{
+    public GameObject CG;
+    public float ThrowingThreshold,Power = 1000; 
+    Vector3 w, w2, e, e2;
+    float ww,v;
+
+    void Start() => v = 5;
+
+    void Update()
+    {
+        v += Time.deltaTime;
+        CG.active = v >= 5;
+        if (v < 5) return;
+        w2 = VRInput.BodyCenterPos.position - e2;
+        w -= w2;
+        ww = Mathf.Abs(w.x) + Mathf.Abs(w.y) + Mathf.Abs(w.z);
+       
+        float data = ww * ww;
+        if (data > ThrowingThreshold)
+        {
+            var x = Instantiate(Resources.Load("Mk2"), transform.position, transform.rotation) as GameObject;
+            x.GetComponent<Rigidbody>().AddRelativeForce(-x.transform.up * Power);
+            v = 0;
+        }
+        e2 = VRInput.BodyCenterPos.position;
+    }
+}
+
