e4fdd604e26cf777f35650813a37aed9c008c181
園田 <e184223sk@kurume-it.ac.jp>
2021-07-30 10:16:32 +0900

    GameSceneのステージとスポーン位置調整

---
+++ Assets/snd/Scripts/PlayerSponePos.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSponePos : MonoBehaviour
+{
+    public Vector3 SponePosition = new Vector3(125, 111, 160);
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GameObject.Find("Player").transform.position = SponePosition;
+    }
+
+}
