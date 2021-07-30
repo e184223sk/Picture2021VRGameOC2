fe3599daa3b78916b1b28d1a75bb110688e6b818
ikeda_ <ikeda.yuichiro.w.00@gmail.com>
2021-07-30 18:19:15 +0900

    ハード系や細かい部分

---
+++ Assets/HardWare_Systems/HW_LSETsys.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class HW_LSETsys : MonoBehaviour
+{
+    void Start()
+    {
+        var vive = GameObject.Find("[HardWare - System]").GetComponent<ViveSystem>();
+        vive.COM = 1;
+        vive.init();
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Lobby");
+    }
+}
